252ff79166902bf35e33194e5648c6aa3e3e3685
githubdood21 <protectdood@gmail.com>
2023-11-04 14:04:51 +0100

    Initial

---
+++ src/main/java/mk/finki/ukim/mk/lab/Lab1bApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.mk.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1bApplication.class, args);
+    }
+
+}
